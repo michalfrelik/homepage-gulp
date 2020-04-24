76d8df4fb43588d2a850ce2acf7ae762ce8e6ec7
Michal Frelik <michal.frelik1@gmail.com>
2020-04-24 13:23:28 +0200

    adjustment to phones

--- src/js/main.js
+++ src/js/main.js
@@ -37,7 +37,7 @@ const navigationSwitcher = document.querySelector('.navigation__switcher--js');
 navigationSwitcher.addEventListener('click', (e) => {
 	const navigationList = document.querySelector('.navigation__list--js');
 	navigationList.classList.toggle('navigation__list--visible');
-	navigationSwitcher.innerHTML = 'XXX';
+	navigationSwitcher.innerHTML = 'X';
 });
 
 
