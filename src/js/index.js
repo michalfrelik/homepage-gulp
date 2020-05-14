a72900c4406b7f229b12bce491f182398976bfc3
Michal Frelik <michal.frelik1@gmail.com>
2020-05-14 12:42:48 +0200

    navigtion switcher repaired + color style changed

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
+"use strict";
+
+// service worker registration - remove if you're not going to use it
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// // place your code below
+
+
+// Nie działają obie funkcje razem
+// - zmiana terści nagłówka
+// - hamburger menubar menu
+// index.html nie pobiera index.js żeby odziselic te fubkcje
 
 const button = document.querySelector('.header__button--js');
 console.log(button);
--- src/js/main.js
+++ src/js/main.js
@@ -36,9 +36,14 @@ const navigationSwitcher = document.querySelector('.navigation__switcher--js');
 
 navigationSwitcher.addEventListener('click', (e) => {
 	const navigationList = document.querySelector('.navigation__list--js');
-	navigationList.classList.toggle('navigation__list--visible');
-	navigationSwitcher.innerHTML = 'X';
+  navigationList.classList.toggle('navigation__list--visible');
+  if (navigationList.classList.contains('navigation__list--visible')) {
+    navigationSwitcher.innerHTML = 'X';
+  } else {
+    navigationSwitcher.innerHTML = 'menu';
+  }
 });
+
 
 
 // const faces = [':D', 'xD', ':O', 'text'];
